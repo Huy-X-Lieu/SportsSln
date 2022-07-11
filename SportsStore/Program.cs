bb87ffb106c06300fde539468b598af78bc0e7ce
Huy Xuon Lieu <huyxuonlieu@gmail.com>
2022-07-11 15:08:36 -0700

    configure entity framework core in the program.cs

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreDbContext>(opts =>
+{
+    opts.UseSqlServer(
+        builder.Configuration["ConnectionStrings:SportStoreConnection"]);
+});
 
 var app = builder.Build();
 
